317de2375291620d06c3252269fc99061e842b78
mirray <mirraymoreirabernardes@gmail.com>
2025-11-06 21:53:16 -0300

    fazendo exerciocs de 1 a 6

---
+++ javascript/subtraçao.js
@@ -0,0 +1,26 @@
+const numero = 22 
+const numero2 = 39
+console.log(numero - numero2)  
+const numero3 = 13
+const numero4 = 7
+
+console.log(numero3 / numero4)
+
+const numero5 = 9
+const numero6 = 14
+
+console.log( numero5 * numero6 )
+
+const numero7 = 14
+
+console.log(numero7 * 2)
+
+console.log(numero2 + numero3 + numero4 / 3)
+
+console.log( numero2 * 1,8 ) 
+
+
+
+
+
+
